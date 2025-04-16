33d1f3a905e56756d8f99039ccfe5e502adafd85
Ivan Kalinichenko <ivan@kalinichenko.dev>
2025-04-16 20:34:50 +0400

    Add Telegram and Twitter icons; update SocialLinks component and consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,8 @@
 export const SITE_TITLE = 'Ivan Kalinichenko – Software Engineer';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const GITHUB_PROFILE_LINK = 'https://github.com/kalinichenko88';
+export const LINKEDIN_PROFILE_LINK = 'https://www.linkedin.com/in/ivan-kalinichenko/';
+export const MAIL_PROFILE_LINK = 'ivan@kalinichenko.dev';
+export const TWITTER_PROFILE_LINK = 'https://twitter.com/kalinichenko88';
+export const TELEGRAM_PROFILE_LINK = 'https://t.me/ivan_kalinichenko';
