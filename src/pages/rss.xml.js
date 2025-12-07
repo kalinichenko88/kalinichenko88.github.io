e987e83be8ec4ddcbc92fcbfd1b4fbbd60d1fbd6
Ivan Kalinichenko <ivan@kalinichenko.dev>
2025-12-07 13:15:44 +0400

    feat: add RSS feed functionality with new endpoint and link in blog header for improved content distribution

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+
+export async function GET(context) {
+  const posts = await getCollection('posts');
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`,
+      categories: post.data.tags,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
