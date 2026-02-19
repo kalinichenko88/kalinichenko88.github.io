b623199850f2ae83c5169af0c744a82cde301760
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-02-19 13:03:12 +0400

    feat(blog): add Giscus comments with theme sync and config constants

--- src/consts.ts
+++ src/consts.ts
@@ -9,3 +9,9 @@ export const LINKEDIN_PROFILE_LINK = 'https://www.linkedin.com/in/ivan-kaliniche
 export const MAIL_PROFILE_LINK = 'ivan@kalinichenko.dev';
 export const TWITTER_PROFILE_LINK = 'https://x.com/joker_bsg';
 export const TELEGRAM_PROFILE_LINK = 'https://t.me/ivan_kalinichenko';
+
+// Giscus (GitHub Discussions comments)
+export const GISCUS_REPO = 'kalinichenko88/kalinichenko88.github.io';
+export const GISCUS_REPO_ID = 'R_kgDOG0lljw';
+export const GISCUS_CATEGORY = 'Blog Comments';
+export const GISCUS_CATEGORY_ID = 'DIC_kwDOG0llj84C2xNt';
