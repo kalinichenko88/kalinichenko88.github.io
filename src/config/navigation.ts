0b3ded372768abcf14883cabf41aa670961faef0
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-04-14 23:35:01 +0400

    feat(navigation): add Projects link to navigation menu

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -5,6 +5,7 @@ type NavLink = {
 
 export const NAV_LINKS: NavLink[] = [
   { href: '/', label: 'Home' },
+  { href: '/#projects', label: 'Projects' },
   { href: '/blog', label: 'Blog' },
   { href: '/#contact', label: 'Contact' },
 ];
